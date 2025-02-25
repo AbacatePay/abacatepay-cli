6b435b32cf928be2a58b9ab73f16ab9dd09e9926
eletroswing <ytalosilvabatalha@gmail.com>
2025-02-24 22:20:04 -0300

    fix(dist): remove wrong added dist

--- dist/index.d.ts
+++
@@ -1,7 +0,0 @@
-declare const yargs: any;
-declare const localtunnel: any;
-declare const express: any;
-declare const createProxyMiddleware: any;
-declare const Express: any;
-declare function getLoggerPrefix(useAbacate?: boolean, useDate?: boolean): string;
-declare function getLogger(useLogger?: boolean, useAbacate?: boolean, useDate?: boolean): (message: string) => void;
