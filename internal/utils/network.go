4f3a0491ae5fdb1d7f1e5e16c73b3602f982aa6e
Victor Albuquerque <albq.victor@gmail.com>
2026-01-13 11:11:52 -0300

    chore(utils): add utility functions to help on developer experience

---
+++ internal/utils/network.go
@@ -0,0 +1,41 @@
+package utils
+
+import (
+	"fmt"
+	"net"
+	"os/exec"
+	"runtime"
+	"time"
+)
+
+func IsOnline() bool {
+	const googleDNS = "8.8.8.8:53"
+	timeout := 2 * time.Second
+	conn, err := net.DialTimeout("tcp", googleDNS, timeout)
+	if err != nil {
+		return false
+	}
+	defer conn.Close()
+	return true
+}
+
+func OpenBrowser(url string) error {
+	var cmd string
+	var args []string
+
+	switch runtime.GOOS {
+	case "windows":
+		cmd = "rundll32"
+		args = []string{"url.dll,FileProtocolHandler", url}
+	case "darwin":
+		cmd = "open"
+		args = []string{url}
+	case "linux":
+		cmd = "xdg-open"
+		args = []string{url}
+	default:
+		return fmt.Errorf("sistema operacional não suportado: %s", runtime.GOOS)
+	}
+
+	return exec.Command(cmd, args...).Start()
+}
