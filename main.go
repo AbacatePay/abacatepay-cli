105eddf0e5771f30874e6a8c70f5eaf3bd3c15d8
Victor Albuquerque <albq.victor@gmail.com>
2026-01-13 23:12:01 -0300

    .

--- main.go
+++ main.go
@@ -11,14 +11,14 @@ import (
 func main() {
 	logCfg, err := logger.DefaultConfig()
 	if err != nil {
-		fmt.Fprintf(os.Stderr, "Erro ao configurar logger: %v\n", err)
+		fmt.Fprintf(os.Stderr, "Error to setup logger: %v\n", err)
 
 		// TODO: Add custom status code later
 		os.Exit(1)
 	}
 
 	if _, err := logger.Setup(logCfg); err != nil {
-		fmt.Fprintf(os.Stderr, "Erro ao inicializar logger: %v\n", err)
+		fmt.Fprintf(os.Stderr, "Error to initializate logger: %v\n", err)
 
 		os.Exit(1)
 	}
