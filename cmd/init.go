12b78048f795552489daa36b9bb92ed7727fc42b
Victor Albuquerque <albq.victor@gmail.com>
2026-01-30 08:34:02 -0300

    feat: add command to initialize new AbacatePay projects

---
+++ cmd/init.go
@@ -0,0 +1,98 @@
+package cmd
+
+import (
+	"fmt"
+
+	"abacatepay-cli/internal/style"
+
+	"github.com/spf13/cobra"
+)
+
+type projectConfig struct {
+	Name       string
+	Framework  string
+	Linter     string
+	BetterAuth bool
+}
+
+var initCmd = &cobra.Command{
+	Use:   "init [project-name]",
+	Short: "Initialize a new AbacatePay project",
+	Args:  cobra.MaximumNArgs(1),
+	RunE: func(cmd *cobra.Command, args []string) error {
+		var name string
+		if len(args) > 0 {
+			name = args[0]
+		}
+		return initializeProject(name)
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(initCmd)
+}
+
+func initializeProject(name string) error {
+	cfg := projectConfig{}
+
+	if name != "" {
+		cfg.Name = name
+	} else {
+		if err := style.Input("Project name", "my-app", &cfg.Name, func(s string) error {
+			if s == "" {
+				return fmt.Errorf("project name is required")
+			}
+			return nil
+		}); err != nil {
+			return err
+		}
+	}
+
+	frameworkOptions := map[string]string{
+		"Next.js": "nextjs",
+		"Elysia":  "elysia",
+	}
+	framework, err := style.Select("Which framework do you want to use?", frameworkOptions)
+	if err != nil {
+		return err
+	}
+	cfg.Framework = framework
+
+	linterOptions := map[string]string{
+		"ESLint": "eslint",
+		"Biome":  "biome",
+	}
+	linter, err := style.Select("Which linter do you want to use?", linterOptions)
+	if err != nil {
+		return err
+	}
+	cfg.Linter = linter
+
+	if err := style.Confirm("Do you want BetterAuth already configured?", &cfg.BetterAuth); err != nil {
+		return err
+	}
+
+	betterAuthLabel := "No"
+	if cfg.BetterAuth {
+		betterAuthLabel = "Yes"
+	}
+
+	frameworkLabel := "Next.js"
+	if cfg.Framework == "elysia" {
+		frameworkLabel = "Elysia"
+	}
+
+	linterLabel := "ESLint"
+	if cfg.Linter == "biome" {
+		linterLabel = "Biome"
+	}
+
+	style.PrintSuccess("Project configured!", map[string]string{
+		"Name":       cfg.Name,
+		"Framework":  frameworkLabel,
+		"Linter":     linterLabel,
+		"BetterAuth": betterAuthLabel,
+	})
+
+	return nil
+}
